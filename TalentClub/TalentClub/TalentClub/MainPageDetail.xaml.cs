6990c6ae45bd2c8ff231fbebc06aed02e3a3a6ca
kirill-hash <atomworld9914@gmail.com>
2020-10-31 13:11:57 +0500

    ф

--- TalentClub/TalentClub/TalentClub/MainPageDetail.xaml.cs
+++ TalentClub/TalentClub/TalentClub/MainPageDetail.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
@@ -12,9 +12,33 @@ namespace TalentClub
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPageDetail : ContentPage
     {
+        public ObservableCollection<string> Items { get; set; }
+
         public MainPageDetail()
         {
             InitializeComponent();
+
+            Items = new ObservableCollection<string>
+            {
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 4",
+                "Item 5"
+            };
+
+            MyListView.ItemsSource = Items;
+        }
+
+        async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item == null)
+                return;
+
+            await DisplayAlert("Item Tapped", "An item was tapped.", "OK");
+
+            //Deselect Item
+            ((ListView)sender).SelectedItem = null;
         }
     }
 }
