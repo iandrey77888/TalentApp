9cf977a0143c03a9df7465f7186e475e99f2f81e
kirill-hash <atomworld9914@gmail.com>
2020-10-31 11:09:56 +0500

    Много

---
+++ TalentClub/TalentClub/TalentClub/MainPageMaster.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using TalentClub.Pages;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TalentClub
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPageMaster : ContentPage
+    {
+        public ListView ListView;
+
+        public MainPageMaster()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainPageMasterViewModel();
+            ListView = MenuItemsListView;
+        }
+
+        class MainPageMasterViewModel : INotifyPropertyChanged
+        {
+            public ObservableCollection<MainPageMasterMenuItem> MenuItems { get; set; }
+
+            public MainPageMasterViewModel()
+            {
+                MenuItems = new ObservableCollection<MainPageMasterMenuItem>(new[]
+                {
+                    new MainPageMasterMenuItem { Id = 0, Title = "Лента", TargetType=typeof(MainPage) },
+                    new MainPageMasterMenuItem { Id = 1, Title = "Личный кабинет", TargetType=typeof(ContentPage) },
+                    new MainPageMasterMenuItem { Id = 2, Title = "Наставники", TargetType=typeof(ContentPage) },
+                    new MainPageMasterMenuItem { Id = 3, Title = "Сообщения", TargetType=typeof(ContentPage) },
+                    new MainPageMasterMenuItem { Id = 4, Title = "Образовательный маршрут", TargetType=typeof(ContentPage) },
+                    new MainPageMasterMenuItem { Id = 5, Title = "Ученики", TargetType=typeof(ContentPage) },
+                    new MainPageMasterMenuItem { Id = 6, Title = "Профориентация", TargetType=typeof(ContentPage) },
+                });
+            }
+
+            #region INotifyPropertyChanged Implementation
+            public event PropertyChangedEventHandler PropertyChanged;
+            void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            {
+                if (PropertyChanged == null)
+                    return;
+
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            #endregion
+        }
+    }
+}
---
+++ TalentClub/TalentClub/TalentClub/Pages/NewsItem.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TalentClub.Pages
+{
+    public class NewsItem
+    {
+        #region Members
+        private Image image;
+        private string text;
+        private DateTime dateTime;
+        #endregion
+        #region Properties
+        public Image NewsImage
+        {
+            get => image;
+            private set
+            {
+                if (value == null)
+                    return;
+                image = value;
+            }
+        }
+        public string NewsText
+        {
+            get => text;
+            private set
+            {
+                if (value == null)
+                    return;
+                text = value;
+            }
+        }
+        public string PublishedTimeString => dateTime.ToShortDateString();
+        public DateTime PublishedTime
+        {
+            get => dateTime;
+            private set
+            {
+                dateTime = value;
+            }
+        }
+        #endregion
+        #region Constructors
+        public NewsItem(Image image, string text)
+        {
+            NewsImage = image;
+            NewsText = text;
+            PublishedTime = DateTime.Now;
+        }
+        public NewsItem(string image_path, string text)
+        {
+            NewsImage = new Image() { Source = image_path };
+            NewsText = text;
+            PublishedTime = DateTime.Now;
+        }
+        #endregion
+    }
+}
